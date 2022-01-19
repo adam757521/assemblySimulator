cf734b8fa48c05b908cd31dc924e000fdc8bcda0
adam7100 <73548219+adam757521@users.noreply.github.com>
2022-01-20 00:52:07 +0200

    Add files via upload

---
+++ src/list/list.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <stdlib.h>
+
+typedef struct
+{
+    void **items;
+    size_t size;
+    size_t used;
+} list_t;
+
+void ListAppend(list_t *self, void *item)
+{
+    if (self->size == self->used)
+    {
+        self->size *= 2;
+        self->items = realloc(self->items, self->size * sizeof(void *));
+    }
+
+    self->items[self->used] = item;
+    self->used++;
+}
+
+void ListFree(list_t *self, int freeItems)
+{
+    if (freeItems)
+    {
+        for (size_t i = 0; i < self->used; i++)
+        {
+            free(self->items[i]);
+        }
+    }
+
+    free(self->items);
+
+    self->items = NULL;
+    self->size = self->used = 0;
+}
+
+list_t CreateDynamicList()
+{
+    list_t list;
+    list.items = malloc(sizeof(void *));
+    list.size = 1;
+    list.used = 0;
+    return list;
+}
